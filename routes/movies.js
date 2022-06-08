50d8f64237662fea8b15ebbc330a4a36c84df3e4
Cristian Alarcon <cristian.alarcon.rodriguez@gmail.com>
2022-06-08 21:16:04 +0200

    it4done

--- routes/movies.js
+++ routes/movies.js
@@ -14,4 +14,16 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+router.get('/:_id', async (req, res, next) =>{
+    const _id = req.params;
+    try{
+        const movieDetail = await Movie.findById(_id);
+        res.render('detail', movieDetail)
+    }
+    catch (error) 
+    {
+        next(error)
+    }
+})
+
 module.exports = router;
